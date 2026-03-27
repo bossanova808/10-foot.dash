c3a240d38838100f6646da4dd4c2fdd7bda07211
bossanova808@gmail.com <jeremy@daalders.net>
2026-03-28 10:55:06 +1100

    NPM update (as far as currently can, issues with moving to Vite 8...)

--- deploy-dev.bat
+++ deploy-dev.bat
@@ -2,6 +2,13 @@
 call deploy-common.bat
 if errorlevel 1 exit /b 1
 
+echo Deploying to: local kodidash
+robocopy /MIR "dist" "C:\mnt\kodidash\home\appdata\nginx\www" /xd ".git"
+if errorlevel 8 (
+    echo Robocopy to kodidash failed!
+    exit /b 1
+)
+
 echo Deploying to: dashdev.bossanova808.net
 robocopy /MIR "dist" "C:\mnt\bossanova808-public-hs14\home\appdata\nginx-dev\www" /xd ".git"
 if errorlevel 8 (
--- deploy.bat
+++ deploy.bat
@@ -2,16 +2,9 @@
 call deploy-common.bat
 if errorlevel 1 exit /b 1
 
-echo Deploying to: kodidash
-robocopy /MIR "dist" "C:\mnt\kodidash\home\appdata\nginx\www" /xd ".git"
+echo Deploying to: dash.bossanova808.net
+robocopy /MIR "dist" "C:\mnt\bossanova808-public-hs14\home\appdata\nginx\www" /xd ".git"
 if errorlevel 8 (
-    echo Robocopy to kodidash failed!
-    exit /b 1
+    echo Robocopy to dash.bossanova808.net failed!
+   exit /b 1
 )
-
-rem echo Deploying to: dash.bossanova808.net
-rem robocopy /MIR "dist" "C:\mnt\bossanova808-public-hs14\home\appdata\nginx\www" /xd ".git"
-rem if errorlevel 8 (
-rem     echo Robocopy to dash.bossanova808.net failed!
-rem    exit /b 1
-rem )
