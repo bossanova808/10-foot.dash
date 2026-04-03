138570b7f61b39db5f51aa603c8b18474160e6ee
bossanova808@gmail.com <jeremy@daalders.net>
2026-04-04 09:32:30 +1100

    Final layout tweaks for V4 release

--- src/js/main.js
+++ src/js/main.js
@@ -101,7 +101,7 @@ Alpine.store('config', {
             this.textSmall = 'text-6xl';
             this.textSmaller = 'text-5xl';
             this.weatherIconSize = 325;
-            this.moonIconSize = 100;
+            this.moonIconSize = 110;
             this.kodiArtworkScale = "max-w-lg"
         }
 
